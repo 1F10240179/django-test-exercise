bb44886dfe242f88a6cf5cd8df919051df65b460
1F10240179 <s1f102401797@iniad.org>
2025-07-03 11:35:39 +0900

    警告を削除6

--- todo/tests.py
+++ todo/tests.py
@@ -3,16 +3,15 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
+
 # Create your tests here.
-
-
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
 
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
-        due=timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
+        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         task = Task(title = 'task1', due_at=due)
         task.save()
         task = Task.objects.get(pk = task.pk)
@@ -31,7 +30,7 @@ class TaskModelTestCase(TestCase):
     def test_is_overdue_future(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         current = timezone.make_aware(datetime(2024, 6, 30, 0, 0, 0))
-        task = Task(title = 'task1',due_at = due)
+        task = Task(title = 'task1', due_at = due)
         task.save()
         self.assertFalse(task.is_overdue(current))
 
